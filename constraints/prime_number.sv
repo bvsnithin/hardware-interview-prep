ceace16f0c0a8c437bb5092aeec95a86be90d57f
bvsnithin <bvsnithin412@tamu.edu>
2026-06-06 13:52:29 -0500

    Add 2 files

--- constraints/prime_number.sv
+++ constraints/prime_number.sv
@@ -11,41 +11,39 @@ Consecutive randomizations should not generate the same prime number.
 class packet;
     rand bit [7:0] num;
     bit [7:0] prev_val;
-    int intialize = 0;
+    int initialize = 0;
 
     constraint c_range{
-        num inside {[2:255]};
+        num inside {[0:255]};
     }
 
     constraint c_not_equals_last_value{
-        if(intialize) num!=prev_val;
+        if(initialize) {
+            prev_val != num;
+        }
     }
 
     constraint c_prime {
-        (num inside {2, 3, 5, 7, 11, 13}) || 
-        (
+        (num inside {2,3,5,7,11,13}) || (
             num > 13 &&
-            num % 2  != 0 &&
-            num % 3  != 0 &&
-            num % 5  != 0 &&
-            num % 7  != 0 &&
-            num % 11 != 0 &&
-            num % 13 != 0
+            num % 2!=0 &&
+            num % 3!=0 &&
+            num % 5!=0 &&
+            num % 7!=0 &&
+            num % 11!=0 &&
+            num % 13!=0
         );
     }
 
     function void post_randomize();
         prev_val = num;
-        intialize = 1;
-
+        initialize = 1;
     endfunction: post_randomize
 
     function int is_prime(input int n);
         if(n<2) return 0;
-        else begin
-            for(int i = 2;i*i<=n;i++) begin
-                if(n%i == 0) return 0;
-            end
+        for(int i =2;i*i<=n;i++) begin
+            if(n%i==0) return 0;
         end
         return 1;
     endfunction: is_prime
@@ -69,3 +67,64 @@ module prime_number;
     end
 
 endmodule: prime_number
+
+
+// class packet;
+//     rand int num;
+//     bit prev_val;
+//     int initialize = 0;
+
+//     constraint c_range{
+//         num inside {[0:255]};
+//     }
+
+//     constraint c_not_equals_last_value{
+//         if(initialize) {
+//             prev_val != num;
+//         }
+//     }
+
+//     constraint c_prime {
+//         (num inside {2,3,5,7,11,13}) || (
+//             num > 13 &&
+//             num % 2!=0 &&
+//             num % 3!=0 &&
+//             num % 5!=0 &&
+//             num % 7!=0 &&
+//             num % 11!=0 &&
+//             num % 13!=0
+//         );
+//     }
+
+//     function void post_randomize();
+//         prev_val = num;
+//         initialize = 1;
+//     endfunction: post_randomize
+
+//     function int is_prime(input int n);
+//         if(n<2) return 0;
+//         for(int i =2;i*i<=n;i++) begin
+//             if(n%i==0) return 0;
+//         end
+//         return 1;
+//     endfunction: is_prime
+
+
+// endclass: packet
+
+// module prime_number;
+
+//     packet p = new();
+
+//     initial begin
+//         repeat(5) begin
+//             if(p.randomize()) begin
+//                 $display("Randomization is successful");
+//                 if(p.is_prime(p.num)) $display("Generated: %0d",p.num);
+//                 else $display("Generated: %0d, but it's not prime",p.num);
+//             end
+//             else $display("Randomization has failed");
+//         end
+//     end
+
+// endmodule: prime_number
