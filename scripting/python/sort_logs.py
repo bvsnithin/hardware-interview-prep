cd4456186ff5953fefad3b46d0cf1d464edf97f7
bvsnithin <bvsnithin412@tamu.edu>
2026-06-20 11:51:57 -0500

    Add some files for python scripting

--- scripting/python/sort_logs.py
+++ scripting/python/sort_logs.py
@@ -9,3 +9,17 @@ Task: Write a script that parses log_file.txt and:
 3. Writes the sorted log lines to a new file named sorted_logs.txt.
 """
 
+try:
+    with open('../practice_files/log_file.txt','r') as fp, open('../practice_files/log_file.txt','w') as new_fp:
+        severity_level = {'ERROR':1, 'WARNING':2, 'INFO':3}
+        logs = []
+        for log in fp:
+            severity = log.split(" - ")[1]
+            logs.append((severity_level(severity), log))
+        logs.sort(key = lambda x:x[0])
+        for level,log in logs:
+            new_fp.write(log)
+            
+        
+except FileNotFoundError as e:
+    print(f"File not found error; {e}")
--- scripting/python/sort_students.py
+++ scripting/python/sort_students.py
@@ -6,4 +6,17 @@ Task: Write a script that parses the student data and:
 3. If two students have the same score, sort them alphabetically by their "Name".
 4. Prints the sorted names and scores to the console.
 """
+import csv
+try:
+    with open('../practice_files/students.csv','r') as fp:
+        reader = csv.reader(fp)
+        header = next(reader)
+        students_data = []
+        for data in reader:
+            students_data.append(data)
+        students_data.sort(key = lambda x: (-1*int(x[4]), x[0]))  #For multiple sorting criteria, return a tuple
+        for data in students_data:
+            print(data)
 
+except FileNotFoundError as e:
+    print(f"File not found error: {e}")
