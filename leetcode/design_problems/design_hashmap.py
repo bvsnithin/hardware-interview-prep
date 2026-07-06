234b74b00f6300a46d074e407fe3bdd79add95f0
bvsnithin <bvsnithin412@tamu.edu>
2026-07-06 13:33:36 -0500

    Add design hashmap and hashset questions in python

---
+++ leetcode/design_problems/design_hashmap.py
@@ -0,0 +1,24 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.map={}
+        
+
+    def put(self, key: int, value: int) -> None:
+        self.map[key] = value
+
+    def get(self, key: int) -> int:
+        return self.map.get(key,-1)
+
+    def remove(self, key: int) -> None:
+        try:
+            del self.map[key]
+        except KeyError as k:
+            return
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
