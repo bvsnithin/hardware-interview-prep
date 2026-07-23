ae619e56d6899c43864b3f04351a3df63191af0d
bvsnithin <bvsnithin412@tamu.edu>
2026-07-22 19:10:57 -0500

    Generate odd parity data constraints question

---
+++ constraints/new_questions/odd_parity.sv
@@ -0,0 +1,36 @@
+/**********************
+Write a function that generates a random 8-bit value with odd parity, 
+where the value must not repeat within the last 4 generations.
+***********************/
+
+class packet_odd_parity;   
+    rand bit[7:0] data;
+    bit[7:0] queue[$];
+
+    constraint c_odd_parity{
+        ^data == 1'b1;
+    }
+    constraint c_not_in_last_4{
+        !(data inside {queue});
+    }
+
+    function void post_randomize();
+        queue.push_back(data);
+        if(queue.size()>4) begin
+            queue.pop_front();
+        end
+    endfunction: post_randomize
+endclass: packt_odd_parity
+
+module test;
+    packet_odd_parity p;
+
+    initial begin
+        repeat(5) begin
+            p = new();
+            if(p.randomize()) begin
+                $display("%08b", p.data);
+            end
+        end
+    end
+endmodule
