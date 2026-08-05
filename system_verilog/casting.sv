e7b6a6ed7766e339b4088627c09bbabfadfce9fc
bvsnithin <bvsnithin412@tamu.edu>
2026-08-05 11:34:53 -0500

    Update casting comments

--- system_verilog/casting.sv
+++ system_verilog/casting.sv
@@ -41,7 +41,8 @@ module test;
         // Upcasting happens implicitly because a Child is a Parent
 
         c = new();
-        p = c;
+        p = c; // This is upcasting. We are assigning a child object to a parent handle. 
+        // p is now pointing to the location in memory where child object is present. Both p and c and pointing to the same location
 
         c.display();  // Will print "Child Display is called!" because we called the display() on child handle pointed to child object
 
@@ -57,7 +58,7 @@ module test;
 
 
         // Downcasting
-        // Here downcasting will fail
+        // Below is an example that demonstrates when downcasting will fail
         $display("\n--------------------------\n");
         p1 = new();  // This creates a Parent object
         if ($cast(c1, p1)) begin
@@ -69,15 +70,17 @@ module test;
 
          $display("\n--------------------------\n");
 
-        //This is how to do successful downcasting. 
+        // This is how to do successful downcasting. 
         // Step 1: Create a Child object using a Child handle
         c1 = new();   
-        // Step 2: Store it in a Parent handle (upcast — always works)
+        // Step 2: Store it in a Parent handle (upcast — always works). Now p1 points to child object
         p1 = c1;      
         // Step 3: Forget about c1 — pretend we only have p1
         c1 = null;    
 
-        // Step 4: Now downcast — "is the thing inside p1 actually a Child?"
+        // Step 4: Now downcast — "is the thing inside p1 actually a Child?" $cast(x,y) check if y points to an object of handle type x
+        // If x is child and y points to a child object, then $cast is successfull. 
+        // If you look back at the previous usage of $cast above, p1 points to Parent Object and c1 is of type Child, and hence cast failed as p1 is not pointing to child object.
         if ($cast(c1, p1)) begin
             $display("Downcast successful! p1 was actually a Child");
             c1.display();
